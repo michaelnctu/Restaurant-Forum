57ed13e8c07a4eb502c4b06d0570881f0f024ad5
MIchael Huang <michaelnctu@gmail.com>
2020-09-08 15:53:40 +0800

    Initialize project

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const app = express()
+const handlebars = require('express-handlebars')
+const port = 3000
+
+app.engine('handlebars', handlebars())
+app.set('view engine', handlebars)
+
+
+app.listen(port, () => {
+  console.log('Example all listening on port ${port}!')
+})
