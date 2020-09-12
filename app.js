91998e85fc57ccd6d6e9689f0585f1a6580fa104
MIchael Huang <michaelnctu@gmail.com>
2020-09-12 18:29:37 +0800

    main.handlebars done

--- app.js
+++ app.js
@@ -4,38 +4,38 @@ const handlebars = require('express-handlebars')
 const bodyParser = require('body-parser')
 const port = process.env.PORT || 3000
 const Handlebars = require('handlebars')
-
-// if (process.env.NODE_ENV !== 'production') {
-//   require('dotenv').config()
-// }
-
-
 const flash = require('connect-flash')
 const session = require('express-session')
 const passport = require('./config/passport')
 const methodOverride = require('method-override')
 
-app.use(methodOverride('_method'))
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
+
 
 
 app.engine('handlebars', handlebars({ defaultLayout: 'main' }))
-app.set('view engine', 'hbs')
-
 app.set('view engine', 'handlebars')
-app.use(bodyParser.urlencoded({ extended: true }))
 
+app.use(bodyParser.urlencoded({ extended: true }))
 app.use(session({ secret: 'secret', resave: false, saveUninitialized: false }))
+
+app.use(passport.initialize())
+app.use(passport.session())
 app.use(flash())
+app.use(methodOverride('_method'))
+
 
 app.use((req, res, next) => {
-  res.locals.success_message = req.flash('success_messages')
+  res.locals.success_messages = req.flash('success_messages')
   res.locals.error_messages = req.flash('error_messages')
   res.locals.user = req.user
   next()
 })
 
-app.use(passport.initialize())
-app.use(passport.session())
+
+
 
 app.use('/upload', express.static(__dirname + '/upload'))
 
