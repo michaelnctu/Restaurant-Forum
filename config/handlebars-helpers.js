f6ef61dec517f13b3ba69d19d1f822dcba3c2e35
MIchael Huang <michaelnctu@gmail.com>
2020-09-15 14:10:29 +0800

    handlebars helper debugging

--- config/handlebars-helpers.js
+++ config/handlebars-helpers.js
@@ -4,5 +4,11 @@ module.exports = {
       return options.fn(this)
     }
     return options.inverse(this)
+  },
+
+
+  selfWareness: function (v1, v2) { //提醒當下的登入情況 
+    if (v1 === v2) { return '(yourself)' }
   }
+
 }
