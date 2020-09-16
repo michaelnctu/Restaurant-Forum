97f56f5a6304b9e59207ff8d72f8cbd446b3ff67
MIchael Huang <michaelnctu@gmail.com>
2020-09-16 13:03:59 +0800

    pagination done

--- controllers/restController.js
+++ controllers/restController.js
@@ -1,24 +1,33 @@
 const db = require('../models')
 const Restaurant = db.Restaurant
 const Category = db.Category
+const pageLimit = 10
 
 
 let restController = {
   getRestaurants: (req, res) => {
-
+    let offset = 0
     let whereQuery = {}
     let categoryId = ''
-
+    if (req.query.page) {
+      offset = (req.query.page - 1) * pageLimit
+    }
     if (req.query.categoryId) {
       categoryId = Number(req.query.categoryId)
-      whereQuery['CategoryId'] = categoryId //{CategoryId: categoryId}
+      whereQuery['categoryId'] = categoryId
     }
-
-    Restaurant.findAll({ include: Category, where: whereQuery }).then(restaurants => {
-
-      const data = restaurants.map(r => ({
-        ...r.dataValues, //展開 
-        description: r.dataValues.description.substring(0, 50)
+    Restaurant.findAndCountAll({ include: Category, where: whereQuery, offset: offset, limit: pageLimit }).then(result => {
+      // data for pagination
+      let page = Number(req.query.page) || 1
+      let pages = Math.ceil(result.count / pageLimit)
+      let totalPage = Array.from({ length: pages }).map((item, index) => index + 1)
+      let prev = page - 1 < 1 ? 1 : page - 1
+      let next = page + 1 > pages ? pages : page + 1
+      // clean up restaurant data
+      const data = result.rows.map(r => ({
+        ...r.dataValues,
+        description: r.dataValues.description.substring(0, 50),
+        categoryName: r.dataValues.Category.name
       }))
       Category.findAll({
         raw: true,
@@ -27,14 +36,14 @@ let restController = {
         return res.render('restaurants', {
           restaurants: data,
           categories: categories,
-          categoryId: categoryId
+          categoryId: categoryId,
+          page: page,
+          totalPage: totalPage,
+          prev: prev,
+          next: next
         })
       })
-
-
     })
-
-
   },
 
   getRestaurant: (req, res) => {
