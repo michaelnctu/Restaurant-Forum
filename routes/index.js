7fec2d8e26efac515825aac9359a080233c4cb6a
MIchael Huang <michaelnctu@gmail.com>
2020-09-17 16:50:52 +0800

    feeds feature

--- routes/index.js
+++ routes/index.js
@@ -35,8 +35,9 @@ module.exports = (app, passport) => {
 
   app.get('/restaurants', authenticated, restController.getRestaurants)
 
-  app.get('/restaurants/:id', authenticated, restController.getRestaurant)
+  app.get('/restaurants/feeds', authenticated, restController.getFeeds)
 
+  app.get('/restaurants/:id', authenticated, restController.getRestaurant)
 
   app.post('/comments', authenticated, commentCategory.postComment)
 
